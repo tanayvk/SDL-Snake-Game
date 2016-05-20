7cd99edd2a28d105a7d7d860c20e8b0df9fb0f77
Tanay Karnik <karniktanay@outlook.com>
2016-05-20 12:02:50 +0530

    implemented the Snake class

---
+++ Source Files/Headers/Snake.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <vector>
+#include "Position.hpp"
+#include "PixelMap.hpp"
+
+using namespace std;
+
+class Snake
+{
+public:
+	// The direction enum
+	enum Direction { Up = -2 , Left, Right, Down };
+	
+	// Move ahead in the current direction
+	bool MoveAhead();
+	// Increase the size of the snake
+	void IncreaseSize();
+	// Change the direction of the snake
+	void ChangeDirection(Direction direction);
+
+	// Get the position of the snake
+	vector<Position> GetPosition() { return myBodyPosition; }
+	// Get the direction of the snake
+	Direction GetDirection() { return myDirection; }
+
+	Snake(vector<Position> position, Direction direction);
+private:
+	// The size of the snake
+	int mySize;
+	// The direction at which the snake is facing
+	Direction myDirection;
+
+	// The position of the last piece
+	Position lastPiece;
+
+	// The vector storing the position of the body of the snake
+	vector<Position> myBodyPosition;
+};
---
+++ Source Files/Source/Snake.cpp
@@ -0,0 +1,59 @@
+#include "..\Headers\Snake.hpp"
+
+bool Snake::MoveAhead()
+{
+	// Set the position of the last piece
+	lastPiece = myBodyPosition[myBodyPosition.size() - 1];
+
+	// Swap all pieces to the front
+	for (int i = myBodyPosition.size(); i > 1; i--)
+	{
+		myBodyPosition[i] = myBodyPosition[i - 1];
+	}
+
+	// Get the value of the direction
+	int directionValue = (int)myDirection;
+
+	// The direction of the snake is vertical (divisible by 2)
+	Position snakeHeadPosition = myBodyPosition[1];
+	
+	// Set the new position of the snake head
+	if (directionValue % 2 == 0)
+		snakeHeadPosition.y += directionValue / 2;
+	else
+		snakeHeadPosition.x += directionValue;
+
+	myBodyPosition[0] = snakeHeadPosition;
+
+	return true;
+}
+
+void Snake::IncreaseSize()
+{
+	if (&lastPiece != NULL)
+	{
+		// Increase the size variable
+		mySize++;
+
+		// Add one more piece to the body
+		myBodyPosition.push_back(lastPiece);
+	}
+}
+
+void Snake::ChangeDirection(Direction direction)
+{
+	myDirection = direction;
+}
+
+Snake::Snake(vector<Position> position, Direction direction)
+{
+	// Set the size of the snake
+	mySize = position.size();
+
+	// Set the position of the snake
+	myBodyPosition = position;
+
+	// Set the direction of the snake
+	myDirection = direction;
+}
+
