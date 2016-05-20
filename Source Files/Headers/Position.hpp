73aa5e2834d303c811163938500689aacdfb7c93
Tanay Karnik <karniktanay@outlook.com>
2016-05-20 11:23:04 +0530

    Implementel the Position class (Helping Class).

---
+++ Source Files/Headers/Position.hpp
@@ -0,0 +1,11 @@
+#pragma once
+class Position
+{
+public:
+	int myX;
+	int myY;
+
+	Position();
+	Position(int x, int y);
+};
+
---
+++ Source Files/Source/Position.cpp
@@ -0,0 +1,15 @@
+#include "..\Headers\Position.hpp"
+
+
+
+Position::Position()
+{
+	myX = 0;
+	myY = 0;
+}
+
+Position::Position(int x, int y)
+{
+	myX = x;
+	myY = y;
+}
